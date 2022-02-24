485afc296d5bc5b9312d816aaf2796162a123ed5
liumenghui <liumenghui@genomics.cn>
2022-02-24 17:27:29 +0800

    增加基础函数练习

---
+++ 30days/element.py
@@ -0,0 +1,16 @@
+import this
+
+a = 1
+b = 2.9
+c = 'hello'
+d = True
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+# 类型转换
+def HuaToCel(input):
+    return (input - 32) / 1.8
+
